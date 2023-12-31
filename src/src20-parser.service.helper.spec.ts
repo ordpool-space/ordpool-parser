092ae5e0b01396625194ad5485e22d732aa1c0bd
Hans Crypto <johannes@haushoppe.art>
2023-12-31 08:54:17 +0100

    cleanup / refactoring

--- src/src20-parser.service.helper.spec.ts
+++ src/src20-parser.service.helper.spec.ts
@@ -1,1 +1,22 @@
+import { bytesToHex, hexToBytes } from "./src20-parser.service.helper";
 
+
+describe('hexToBytes', () => {
+  it('should convert a hexadecimal string to an array of bytes', () => {
+    expect(hexToBytes('')).toEqual([]);
+    expect(hexToBytes('00')).toEqual([0]);
+    expect(hexToBytes('ff')).toEqual([255]);
+    expect(hexToBytes('00ff')).toEqual([0, 255]);
+    expect(hexToBytes('000102')).toEqual([0, 1, 2]);
+    expect(hexToBytes('abcdef')).toEqual([171, 205, 239]);
+  });
+});
+
+it('converts an array of bytes to a hexadecimal string', () => {
+  expect(bytesToHex([])).toEqual('');
+  expect(bytesToHex([0])).toEqual('00');
+  expect(bytesToHex([255])).toEqual('ff');
+  expect(bytesToHex([0, 255])).toEqual('00ff');
+  expect(bytesToHex([0, 1, 2])).toEqual('000102');
+  expect(bytesToHex([171, 205, 239])).toEqual('abcdef');
+});
--- src/src20-parser.service.helper.ts
+++ src/src20-parser.service.helper.ts
@@ -1,55 +1,85 @@
-export function hexToBytes(a: string): number[] {
-  for (var b = [], c = 0; c < a.length; c += 2)
-      b.push(parseInt(a.substr(c, 2), 16));
-  return b;
+import { OP_PUSHDATA1, OP_PUSHDATA2, OP_PUSHDATA4, readPushdata } from "./inscription-parser.service.helper";
+
+/**
+ * Converts a hexadecimal string to an array of numbers.
+ *
+ * Each pair of hexadecimal characters (00 through FF) is converted to its
+ * corresponding byte value, which is represented as a number (0 through 255).
+ * The function iterates through the hexadecimal string two characters at a time,
+ * parsing each pair into a byte and adding it to the resulting array.
+ *
+ * @param hexStr - A string of hexadecimal characters.
+ * @returns An array of numbers, where each number is a byte value (0-255) corresponding to a pair of hex characters.
+ */
+export function hexToBytes(hexStr: string): number[] {
+  const bytes = [];
+  for (let i = 0; i < hexStr.length; i += 2) {
+    bytes.push(parseInt(hexStr.substr(i, 2), 16));
+  }
+  return bytes;
 }
 
-export function bytesToHex(a: number[]): string {
-  for (var b = [], c = 0; c < a.length; c++)
-      b.push((a[c] >>> 4).toString(16)),
-      b.push((a[c] & 15).toString(16));
-  return b.join("")
+/**
+ * Converts an array of byte values (numbers) to a hexadecimal string.
+ *
+ * Each number in the array is assumed to be a byte (0 through 255) and is
+ * converted to its corresponding two-character hexadecimal representation.
+ * The function iterates over each number in the array, converts it to
+ * hexadecimal, and concatenates the result into a single string.
+ *
+ * @param byteArray - An array of numbers, each representing a byte.
+ * @returns A string of hexadecimal characters representing the byte array.
+ */
+export function bytesToHex(byteArray: number[]): string {
+  const hexArray = [];
+  for (let i = 0; i < byteArray.length; i++) {
+    hexArray.push((byteArray[i] >>> 4).toString(16));
+    hexArray.push((byteArray[i] & 0xF).toString(16));
+  }
+  return hexArray.join('');
 }
 
-export function parseScript (buffer:  number[]): Array<number | number[]> {
+/**
+ * Determines if an opcode represents a data push operation.
+ *
+ * @param {number} opcode - The opcode to evaluate.
+ * @returns {boolean} True if the opcode represents a data push.
+ */
+function isDataPushOpcode(opcode: number): boolean {
+  // Opcodes from 0x01 to 0x4b (decimal values 1 to 75) are special opcodes that indicate a data push is happening.
+  return (0x01 <= opcode && opcode <= 0x4b) ||
+         (opcode === OP_PUSHDATA1 ||
+          opcode === OP_PUSHDATA2 ||
+          opcode === OP_PUSHDATA4);
+}
 
+/**
+ * Parses a Bitcoin script and extracts its components (opcodes and data).
+ *
+ * @param {number[]} buffer - An array of numbers representing the script in bytes.
+ * @returns {Array<number | number[]>} An array where each element is either an opcode or an array of data bytes.
+ */
+export function parseScript(buffer: number[]): Array<number | number[]> {
   const chunks: Array<number | number[]> = [];
-  var i = 0;
-
-  function readChunk(n: number) {
-    chunks.push(buffer.slice(i, i + n));
-    i += n;
-  };
+  let i = 0;
 
   while (i < buffer.length) {
-    var opcode = buffer[i++];
-    if (opcode >= 0xF0) {
-        opcode = (opcode << 8) | buffer[i++];
-    }
+    const opcode = buffer[i];
 
-    var len;
-    if (opcode > 0 && opcode < 76) { //OP_PUSHDATA1
-      readChunk(opcode);
-    } else if (opcode == 76) { //OP_PUSHDATA1
-      len = buffer[i++];
-      readChunk(len);
-    } else if (opcode == 77) { //
-      len = (buffer[i++] << 8) | buffer[i++];
-      readChunk(len);
-    } else if (opcode == 78) { //OP_PUSHDATA4
-      len = (buffer[i++] << 24) | (buffer[i++] << 16) | (buffer[i++] << 8) | buffer[i++];
-      readChunk(len);
+    if (isDataPushOpcode(opcode)) {
+      // Use readPushdata for handling data push opcodes
+      const [data, newPointer] = readPushdata(new Uint8Array(buffer), i);
+      chunks.push(Array.from(data)); // Convert Uint8Array back to number[]
+      i = newPointer;
     } else {
+      // Handle non-push opcodes
       chunks.push(opcode);
-    }
-
-    if(i<0x00){
-      break;
+      i++;
     }
   }
 
   return chunks;
-};
+}
 
 
 /**
