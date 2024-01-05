5c6cd2bda7c9cecb1f75cc0f90bc65e181d2f087
Hans Crypto <johannes@haushoppe.art>
2024-01-06 00:17:47 +0100

    + cat-lasereye-poses-testdrive.html

--- src/cat21-parser.service.spec.ts
+++ src/cat21-parser.service.spec.ts
@@ -5,6 +5,26 @@ import { Cat21ParserService } from './cat21-parser.service';
 import { readTransaction } from './test.helper';
 import { MooncatParser } from './lib/mooncat-parser';
 
+
+const testdriveHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Testing Cats</title>
+
+    <style>
+      svg {
+        width: 50px;
+        border: 1px solid silver;
+        display: block;
+        float: left;
+      }
+    </style>
+  </head>
+  <body>
+    CATS!
+  </body>
+</html>`
+
 describe('Cat21ParserService', () => {
 
   const baseTxn = {
@@ -37,35 +57,64 @@ describe('Cat21ParserService', () => {
 
   it('should render all potential cats of a block!', () => {
 
-    let stopAt = 1000;
     let svgContent = '';
     for (let i = 0; i < txIds.length; i++) {
       svgContent += MooncatParser.generateMoonCatSvg(txIds[i]);
-      if (i >= stopAt) { break; }
+      if (i >= 1000) { break; }
     }
 
-    const finalHtml = `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Testing Cats</title>
+    fs.writeFileSync('testdist/cat-block-testdrive.html', testdriveHtml.replace('CATS!', svgContent));
+  });
 
-    <style>
-      svg {
-        max-width: 100px;
-        max-height: 100px;
-        border: 1px solid silver;
-        display: block;
-        float: left;
+  it('should generate examples with laser eyes in all the possible palettes', () => {
+
+    // const steps = [0, 28, 56, 84, 112, 140, 168, 196, 224, 255];
+    const steps = [0, 51, 102, 153, 204, 255];
+
+    let svgContent = '';
+
+    for (let k = 0; k < steps.length; k++) {
+      for (let r = 0; r < steps.length; r++) {
+        for (let g = 0; g < steps.length; g++) {
+
+          for (let b = 0; b < steps.length; b++) {
+
+            const txId = '00' +
+              steps[k].toString(16).padStart(2, '0') +
+              steps[r].toString(16).padStart(2, '0') +
+              steps[g].toString(16).padStart(2, '0') +
+              steps[b].toString(16).padStart(2, '0') +
+              (224).toString(16).padStart(2, '0');
+
+            svgContent += `<span title="${k} | ${r} | ${g} | ${b}">` + MooncatParser.generateMoonCatSvg(txId) + '</span>';
+          }
+
+          svgContent += '<br style="clear:both">'
+        }
+        svgContent += `<br style="clear:both">`
       }
-    </style>
-  </head>
-  <body>
-    ${svgContent}
-  </body>
-</html>
-</svg>`;
+      svgContent += `<br style="clear:both">`
+    }
 
+    fs.writeFileSync('testdist/cat-lasereye-palettes-testdrive.html', testdriveHtml.replace('CATS!', svgContent));
+  });
 
-    fs.writeFileSync('testdist/cat-testdrive.html', finalHtml);
+  it('should generate examples with laser eyes in all poses', () => {
+
+    let svgContent = '';
+
+    for (let k = 0; k < 256; k++) {
+
+      const txId = '00' +
+        k.toString(16).padStart(2, '0') +
+        (0).toString(16).padStart(2, '0') +
+        (3).toString(16).padStart(2, '0') +
+        (4).toString(16).padStart(2, '0') +
+        (224).toString(16).padStart(2, '0');
+
+      svgContent += `<span title="${k}">` + MooncatParser.generateMoonCatSvg(txId) + '</span>';
+    }
+
+    fs.writeFileSync('testdist/cat-lasereye-poses-testdrive.html', testdriveHtml.replace('CATS!', svgContent));
   });
 });
