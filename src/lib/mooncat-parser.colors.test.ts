8180830ca8788cea9acaf08b673eee53140be70b
Hans Crypto <johannes@haushoppe.art>
2024-03-05 23:56:04 +0100

    me, trying to understand what @Ethspresso did

---
+++ src/lib/mooncat-parser.colors.test.ts
@@ -0,0 +1,81 @@
+import fs from 'fs';
+
+import { generativeColorPalette } from "./mooncat-parser.colors";
+import { RGBToHex, derivePalette } from './mooncat-parser.helper';
+
+describe('colors', () => {
+
+  it('should use a phase shifting palette (Ethspresso version)', () => {
+
+    let colors: (string | null)[];
+
+    // --- ethspresso version
+
+    let htmlEthspresso = '';
+    for (let feeRate = 0; feeRate < 1000; feeRate++) {
+
+      const x = feeRate / 300;
+      const rgb = generativeColorPalette(
+        x,
+        [0.5, 0.5, 0.5],
+        [-0.9, 0.6, 0.4],
+        [2.0, 1.0, 1.0],
+        [0.0, 0.0, 0.0]
+      );
+
+      // If feeRate is less than 150, the palette will emphasize variations in the red and green channels,
+      // resulting in colors that primarily vary along these axes.
+      if (feeRate < 150) {
+        colors = derivePalette(rgb[0], rgb[1], 0);
+
+      // If feeRate is greater than or equal to 150, the palette will emphasize variations in the red and blue channels,
+      // leading to colors that primarily vary along these axes instead.
+      } else {
+        colors = derivePalette(rgb[0], 0, rgb[2]);
+      }
+
+      htmlEthspresso += `Fee rate ${(feeRate + '').padStart(3, ' ')} / ${ x.toFixed(3) }: `
+        + `<span style="color:${colors[1]}">${colors[1]}</span> `
+        + `<span style="color:${colors[2]}">${colors[2]}</span> `
+        + `<span style="color:${colors[3]}">${colors[3]}</span> `
+        + `<span style="color:${colors[4]}">${colors[4]}</span> `
+        + `<span style="color:${colors[5]}">${colors[5]}</span>\n`
+    }
+
+    // --- rainbow version to understand the algo
+
+    let html2 = '';
+    for (let feeRate = 0; feeRate < 1000; feeRate++) {
+
+      const x = feeRate / 100;
+      const rgb = generativeColorPalette(
+        x,
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [1.0, 1.0, 1.0],
+        [0.00, 0.33, 0.67]
+      );
+
+      colors = derivePalette(rgb[0], rgb[1], rgb[2]);
+
+      html2 += `Fee rate ${(feeRate + '').padStart(3, ' ')} / ${ x.toFixed(3) }: `
+      + `<span style="color:${colors[1]};font-weight:bold;">${colors[1]}</span> `
+      + `<span style="color:${colors[2]};font-weight:bold;">${colors[2]}</span> `
+      + `<span style="color:${colors[3]};font-weight:bold;">${colors[3]}</span> `
+      + `<span style="color:${colors[4]};font-weight:bold;">${colors[4]}</span> `
+      + `<span style="color:${colors[5]};font-weight:bold;">${colors[5]}</span>\n`
+    }
+
+
+    const finalHtml = `<html>
+    <table><tr>
+      <td><pre>${htmlEthspresso}</pre></td>
+      <td>&nbsp;</td>
+      <td><pre>${html2}</pre></td>
+    </html>`
+
+    expect(finalHtml).toBeTruthy();
+
+    fs.writeFileSync('testdist/palette-testdrive.html', finalHtml);
+  });
+});
--- src/lib/mooncat-parser.colors.ts
+++ src/lib/mooncat-parser.colors.ts
@@ -1,9 +1,18 @@
-// Generative color palette based on an approach by Inigo Quilez
-// Typescript implementation of a GLSL method described at https://iquilezles.org/articles/palettes/
-export function generativeColorPalette(x: number, a: number[], b: number[], c: number[], d: number[]): number[] {
+/**
+ * Generative color palette based on an approach by Inigo Quilez
+ * Typescript implementation of a GLSL method described at https://iquilezles.org/articles/palettes/
+ *
+ * @param t Normalized palette index (0 to 1).
+ * @param baseColor Base color of the palette (RGB values).
+ * @param amplitude Amplitude of color variation (RGB values).
+ * @param frequency Frequency of color variation (RGB values).
+ * @param phase Phase offset of color variation (RGB values).
+ * @returns An array of RGB color values.
+ */
+export function generativeColorPalette(t: number, baseColor: number[], amplitude: number[], frequency: number[], phase: number[]): number[] {
   return [
-      a[0] + b[0] * Math.cos(2*Math.PI * (c[0] * x + d[0])),
-      a[1] + b[1] * Math.cos(2*Math.PI * (c[1] * x + d[1])),
-      a[2] + b[2] * Math.cos(2*Math.PI * (c[2] * x + d[2]))
+      baseColor[0] + amplitude[0] * Math.cos(2*Math.PI * (frequency[0] * t + phase[0])),
+      baseColor[1] + amplitude[1] * Math.cos(2*Math.PI * (frequency[1] * t + phase[1])),
+      baseColor[2] + amplitude[2] * Math.cos(2*Math.PI * (frequency[2] * t + phase[2]))
   ];
 }
