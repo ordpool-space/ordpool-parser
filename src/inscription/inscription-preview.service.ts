a2b6a1b462116c54671da8e42d8a715cf514071b
Hans Crypto <johannes@haushoppe.art>
2024-11-26 14:42:02 +0100

    formated preview for code and json

--- src/inscription/inscription-preview.service.ts
+++ src/inscription/inscription-preview.service.ts
@@ -1,3 +1,4 @@
+import { binaryStringToBase64, bytesToBinaryString, unicodeStringToBytes } from '../lib/conversions';
 import { ParsedInscription } from '../types/parsed-inscription';
 
 export interface PreviewInstructions {
@@ -24,43 +25,100 @@ export class InscriptionPreviewService {
     if (!inscription) {
       return {
         instructionsFor: undefined,
-        previewContent: getPreviewUnknown(''),
+        previewContent: await getPreviewUnknown(undefined as any),
         renderDirectly: false
       }
     }
 
-    let previewFunction: (dataUri: string) => string = getPreviewUnknown;
+    let previewFunction: (inscription: ParsedInscription) => Promise<string> = getPreviewUnknown;
     if (inscription.contentType && table[inscription.contentType]) {
       previewFunction = table[inscription.contentType];
     }
 
     if (previewFunction === getPreviewIframe) {
-        return {
-          instructionsFor: inscription.inscriptionId,
-          previewContent: '',
-          renderDirectly: true
-        }
+      return {
+        instructionsFor: inscription.inscriptionId,
+        previewContent: '',
+        renderDirectly: true
+      }
     }
 
     if (previewFunction === getPreviewUnknown) {
       return {
         instructionsFor: inscription.inscriptionId,
-        previewContent: getPreviewUnknown(''),
+        previewContent: await previewFunction(inscription),
         renderDirectly: false
       }
     }
 
-    const dataUri = await inscription.getDataUri();
-
     return {
       instructionsFor: inscription.inscriptionId,
-      previewContent: previewFunction(dataUri),
+      previewContent: await previewFunction(inscription),
       renderDirectly: false
     }
   }
+
+  /**
+   * Determines how to display the content of an inscription based on its content type.
+   *
+   * @param inscription - The parsed inscription object, which includes metadata and content.
+   *
+   * @returns A Promise resolving to an object with:
+   *  - `content`: The content of the inscription if it is valid JSON or code; `undefined` otherwise.
+   *  - `whatToShow`: A string indicating the recommended way to display the content:
+   *      - `'json'`: The content is valid JSON and can be rendered in a JSON viewer.
+   *      - `'code'`: The content should be rendered as code (e.g., YAML, CSS, or JavaScript).
+   *      - `'preview'`: The content should be displayed in the preview mode (default case for all other types).
+   *
+   * @remarks
+   * - This function handles content types such as `text/plain`, `application/json`, `application/yaml`, `text/css`, and JavaScript variants.
+   * - If the content type is `text/plain` or `application/json`, it attempts to parse the content as JSON. If valid, it returns `json` as `whatToShow`.
+   * - For content types such as YAML, CSS, or JavaScript, it returns `code` as `whatToShow`.
+   * - For other content types, it defaults to returning `preview`.
+   */
+  static async getContentTypeInstructions(inscription: ParsedInscription): Promise<{
+    content: string | undefined,
+    whatToShow: 'json' | 'code' | 'preview'
+  }> {
+
+    let content: string | undefined = undefined;
+
+    if (inscription.contentType?.startsWith('text/plain') ||
+      inscription.contentType?.startsWith('application/json')) {
+
+        content = await inscription.getContent();
+        const isValidJson = validateJson(content);
+
+        if (isValidJson) {
+          return {
+            content,
+            whatToShow: 'json'
+          };
+        }
+    }
+
+    if (inscription.contentType?.startsWith('application/yaml') ||
+      inscription.contentType?.startsWith('text/css') ||
+      inscription.contentType?.startsWith('text/javascript') ||
+      inscription.contentType?.startsWith('application/x-javascript')) {
+
+      content = content || await inscription.getContent();
+
+      return {
+        content,
+        whatToShow: 'code'
+      };
+    }
+
+    // avoids unnecessary await
+    return {
+      content: undefined,
+      whatToShow: 'preview'
+    };
+  }
 }
 
-const table: { [key: string]: (dataUri: string) => string } = {
+const table: { [key: string]: (inscription: ParsedInscription) => Promise<string> } = {
   'application/cbor': getPreviewUnknown,
   'application/json': getPreviewText,
   'application/octet-stream': getPreviewUnknown,
@@ -99,15 +157,18 @@ const table: { [key: string]: (dataUri: string) => string } = {
   'video/webm': getPreviewVideo,
 };
 
+
+
 // test here: http://localhost:4200/tx/751007cf3090703f241894af5c057fc8850d650a577a800447d4f21f5d2cecde
-function getPreviewIframe(_dataUri: string): string {
+async function getPreviewIframe(_inscription: ParsedInscription): Promise<string> {
   // return decodeDataURI(dataUri);
   return ''
 }
 
 // test here: http://localhost:4200/tx/ad99172fce60028406f62725b91b5c508edd95bf21310de5afeb0966ddd89be3
-function getPreviewAudio(dataUri: string): string {
+async function getPreviewAudio(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -123,8 +184,9 @@ function getPreviewAudio(dataUri: string): string {
 }
 
 // test here http://localhost:4200/tx/6fb976ab49dcec017f1e201e84395983204ae1a7c2abf7ced0a85d692e442799
-function getPreviewImage(dataUri: string): string {
+async function getPreviewImage(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -160,8 +222,9 @@ function getPreviewImage(dataUri: string): string {
 }
 
 // test here: http://localhost:4200/tx/c133c03e2ed44bb8ada79b1640b6649129de75a8f31d8e6ad573ede442f91cdb
-function getPreviewMarkdown(dataUri: string): string {
+async function getPreviewMarkdown(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -176,8 +239,9 @@ function getPreviewMarkdown(dataUri: string): string {
 }
 
 // test here: http://localhost:4200/tx/25013a3ab212e0ca5b3ccbd858ff988f506b77080c51963c948c055028af2051
-function getPreviewModel(dataUri: string): string {
+async function getPreviewModel(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -199,8 +263,9 @@ function getPreviewModel(dataUri: string): string {
 
 // test here: http://localhost:4200/tx/85b10531435304cbe47d268106b58b57a4416c76573d4b50fa544432597ad670i0
 // (shows only the first page)
-function getPreviewPdf(dataUri: string): string {
+async function getPreviewPdf(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -216,8 +281,61 @@ function getPreviewPdf(dataUri: string): string {
 }
 
 // test here: http://localhost:4200/tx/430901147831e41111aced3895ee4b9742cf72ac3cffa132624bd38c551ef379
-function getPreviewText(dataUri: string): string {
+async function getPreviewText(inscription: ParsedInscription): Promise<string> {
 
+  const instructions = await InscriptionPreviewService.getContentTypeInstructions(inscription);
+
+
+  if (instructions.whatToShow === 'json') {
+    const formatedText = instructions.content ? formatJSON(instructions.content) : '';
+
+    const bytes = unicodeStringToBytes(formatedText);
+    const content = bytesToBinaryString(bytes);
+    const fullBase64Data = binaryStringToBase64(content);
+    const dataUri = `data:${inscription.contentType};base64,${fullBase64Data}`;
+
+    // TODO: format with highlight.js
+    return `<!doctype html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <link href='/resources/inscription-assets/preview-text.css' rel='stylesheet'>
+
+    <script>window.textBase64 = '${dataUri}'</script>
+    <script src='/resources/inscription-assets/preview-text-json.js' defer type='module'></script>  </head>
+  <body>
+    <pre><code></code></pre>
+  </body>
+</html>`
+  }
+
+  // TODO: format with prettier
+  if (instructions.whatToShow === 'code') {
+
+    const formatedText = instructions.content || '';
+    const bytes = unicodeStringToBytes(formatedText);
+    const content = bytesToBinaryString(bytes);
+    const fullBase64Data = binaryStringToBase64(content);
+    const dataUri = `data:${inscription.contentType};base64,${fullBase64Data}`;
+
+    return `<!doctype html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <link href='/resources/inscription-assets/preview-text.css' rel='stylesheet'>
+
+    <script>window.textBase64 = '${dataUri}'</script>
+    <script src='/resources/inscription-assets/preview-text-code.js' defer type='module'></script>
+  </head>
+  <body>
+    <pre><code></code></pre>
+  </body>
+</html>`
+  }
+
+
+
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -235,7 +353,7 @@ function getPreviewText(dataUri: string): string {
 }
 
 // test here: http://localhost:4200/tx/06158001c0be9d375c10a56266d8028b80ebe1ef5e2a9c9a4904dbe31b72e01c
-function getPreviewUnknown(_dataUri: string): string {
+async function getPreviewUnknown(_inscription: ParsedInscription): Promise<string> {
 
   return `<!doctype html>
 <html lang='en'>
@@ -250,8 +368,9 @@ function getPreviewUnknown(_dataUri: string): string {
 }
 
 // test here: http://localhost:4200/tx/700f348e1acef6021cdee8bf09e4183d6a3f4d573b4dc5585defd54009a0148c
-function getPreviewVideo(dataUri: string): string {
+async function getPreviewVideo(inscription: ParsedInscription): Promise<string> {
 
+  const dataUri = await inscription.getDataUri();
   return `<!doctype html>
 <html lang='en'>
   <head>
@@ -264,4 +383,31 @@ function getPreviewVideo(dataUri: string): string {
     </video>
   </body>
 </html>`;
+}
+
+/**
+ * Checks if a given string is valid JSON.
+ *
+ * @param str - The string to be tested.
+ * @returns Returns true if the string is valid JSON, otherwise false.
+ */
+export function validateJson(str: string) {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Formats a JSON string with indentation for better readability.
+ *
+ * @param jsonString - The JSON string to be formatted.
+ * @param [indentation=2] - The number of spaces to use for indentation. Default is 4.
+ * @returns The formatted JSON string, or an error message if the input is not valid JSON.
+ */
+export function formatJSON(jsonString: string, indentation = 2) {
+  const parsed = JSON.parse(jsonString);
+  return JSON.stringify(parsed, null, indentation);
 }
